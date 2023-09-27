c5f3feb6cafff43205b3d470388e611368bef17d
Dominic Coetzer <dominicgames161@gmail.com>
2023-09-27 22:12:03 +0100

    Attempt 1

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 const box = document.querySelectorAll('.content3');
+const ingredient_btn = document.querySelectorAll('.ingrediant-btn');
 
 for (let i = 0; i < box.length; i++) {
     box[i].addEventListener('click', function() {
@@ -9,3 +10,12 @@ for (let i = 0; i < box.length; i++) {
         color2.classList.toggle('hidden');
     });
 }; 
+
+
+for (let i = 0; i < ingredient_btn.length; i++) {
+    ingredient_btn[i].addEventListener('click', function() {
+        const img = document.querySelector('.ingrediant-img');
+
+        img.classList.toggle('scaler')
+    })
+} 
